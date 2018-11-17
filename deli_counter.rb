b2ddb9462b9e1f985df664dbe3895b47d03eed52
Mahmoud Agamy <agamyv@gmail.com>
2018-11-17 00:25:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(arr)
+  if arr.length > 0
+    output = "The line is currently:"
+    i = 1
+    while i <= arr.length
+      output << " #{i}. #{arr[i-1]}"
+      i+=1
+    end
+    puts output
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(arr, str)
+  arr << str
+  return arr
+def line
+  
+
+end
+end
